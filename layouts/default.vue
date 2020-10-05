82c57a561e938ae60a74594a6c606ef47f85da0f
Boris MAURENCE <b.maurence@gmail.com>
2020-10-05 21:46:21 +0200

    Layout implementation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,62 @@
 <template>
-  <div>
+  <div class="app">
+    <nav class="navbar is-transparent" role="navigation">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item">
+            <p>
+              Margaux
+              <i>FROWEIN</i>
+            </p>
+          </a>
+          <a
+            role="button"
+            class="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            :class="{ 'is-active': isBurgerToggled }"
+            v-on:click="isBurgerToggled = !isBurgerToggled"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+        <div class="navbar-menu" :class="{ 'is-active': isBurgerToggled }">
+          <div class="navbar-start">
+            <nuxt-link to="/" class="navbar-item has-text-centered"
+              >Home</nuxt-link
+            >
+            <nuxt-link to="/contact" class="navbar-item">Contact</nuxt-link>
+          </div>
+        </div>
+      </div>
+    </nav>
     <Nuxt />
   </div>
 </template>
 
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+<script>
+export default {
+  data() {
+    return {
+      isBurgerToggled: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/main.scss";
+
+.navbar-start {
+  flex-grow: 1;
+  justify-content: center;
+  text-align: center;
 }
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.navbar-item {
+  min-width: 180px;
+  justify-content: center;
 }
 </style>
