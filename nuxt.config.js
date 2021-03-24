52655b19b4ba69eb6e54097466b4f46a90789e92
Boris MAURENCE <b.maurence@gmail.com>
2021-03-24 18:44:42 +0100

    Environment var target for static netlify

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
     ** Nuxt target
     ** See https://nuxtjs.org/api/configuration-target
     */
-    target: 'server',
+    target: process.env.TARGET,
     /*
     ** Headers of the page
     ** See https://nuxtjs.org/api/configuration-head
